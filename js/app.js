d922e1c2e2480119eadbf5659cc6cbaecf794476
yasutakan <yasutaka.nakamura@luminova.co.jp>
2020-05-09 10:13:02 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+      var lastIndex = -1;
+      var IMAGES = ['#images_01', '#images_02', '#images_03'];
+      this.el.addEventListener('click', function (evt) {
+        lastIndex = (lastIndex + 1) % IMAGES.length;
+        this.setAttribute('src', IMAGES[lastIndex]);
+        console.log('I was clicked at: ', evt.detail.intersection.point);
+      });
+    }
+  });
+
+
+  AFRAME.registerComponent('collider-check', {
+    dependencies: ['raycaster'],
+  
+    init: function () {
+      this.el.addEventListener('raycaster-intersection', function () {
+        console.log('Player hit something!');
+      });
+    }
+  });
